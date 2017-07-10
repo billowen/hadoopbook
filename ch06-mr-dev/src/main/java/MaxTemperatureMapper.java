eadec1bf0b659e386eca94c425f7c049505a937c
shaokp <shaokp@outlook.com>
2017-07-10 13:58:44 +0800

    commit

--- ch06-mr-dev/src/main/java/MaxTemperatureMapper.java
+++ ch06-mr-dev/src/main/java/MaxTemperatureMapper.java
@@ -12,6 +12,9 @@ public class MaxTemperatureMapper
         extends Mapper<LongWritable, Text, Text, IntWritable> {
 
     private NcdcRecordParser parser = new NcdcRecordParser();
+    enum Temperature {
+        OVER_100
+    }
     @Override
     public void map(LongWritable key, Text value, Context context)
             throws IOException, InterruptedException {
@@ -26,6 +29,12 @@ public class MaxTemperatureMapper
         //v2
         parser.parse(value);
         if (parser.isValidTemperature()) {
+            int airTemperature = parser.getAirTemperature();
+            if (airTemperature > 1000) {
+                System.err.println("Temperature over 100 degrees for input: " + value);
+                context.setStatus("Detected possibly corrupt record: see logs.");
+                context.getCounter(Temperature.OVER_100).increment(1);
+            }
             context.write(new Text(parser.getYear()), new IntWritable(parser.getAirTemperature()));
         }
     }
